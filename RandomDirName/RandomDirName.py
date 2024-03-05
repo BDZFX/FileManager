1ba114c5adccdcd82bfeeb2e59ff390acbdf72d8
BDZFX <161376933+BDZFX@users.noreply.github.com>
2024-03-05 18:34:10 +0900

    Update RandomDirName.py

--- RandomDirName/RandomDirName.py
+++ RandomDirName/RandomDirName.py
@@ -1,1 +1,18 @@
-print("hello")
+#Use: python3 RandomDirName.py <path> <random_string_length>
+
+import os
+import string
+import random
+import sys
+
+def RandomDirName(target_path, rndstr_len):
+
+  for bef_dir in os.listdir(target_path):
+    aft_dir = ""
+    for i in range(int(rndstr_len)):
+      aft_dir += random.choice(string.ascii_letters)
+
+    bef_abspath = os.path.join(target_path, bef_dir)
+    aft_abspath = os.path.join(target_path, aft_dir)
+
+    os.rename(bef_abspath, aft_abspath)
